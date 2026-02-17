ff7734512bb028376f8f9d041ebb4891783b8a54
Spacy96 <francescoalbin@gmail.com>
2026-02-17 11:43:41 +0100

    pagine

---
+++ index.php
@@ -0,0 +1,1 @@
+<h1>Sito DeBook</h1>
---
+++ login.php
@@ -0,0 +1,9 @@
+<h1> Esegui il login</h1>
+
+<form method="post" action="login_controller.php">
+    Username<br>
+    <input type="text" name="username" required><br>
+    Password<br>
+    <input type="password" name="password" required><br><br>
+    <input type="submit" value="Invio">
+</form>
