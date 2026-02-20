fedf188c0555e1a82d1e93344aa123db600158a3
masalaFrancesco3F <masala.francesco.2023@itisgalileiroma.it>
2026-02-20 13:28:46 +0100

    spazio

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <form action="register.php" method="POST">
     <h2>Crea un Account</h2>
-    <input type="text" name="username" placeholder="Username" required>
-    <input type="email" name="email" placeholder="Email" required>
-    <input type="password" name="password" placeholder="Password" required>
+    <input type="text" name="username" placeholder="Username" required><br>
+    <input type="email" name="email" placeholder="Email" required><br>
+    <input type="password" name="password" placeholder="Password" required><br>
     <button type="submit">Registrati</button>
 </form>
 <?php
